93e5654d10c189f800166ed6586a81f9f4afc7a3
Shocked-Cat <dfu5757@gmail.com>
2026-02-11 02:07:50 +0100

    Improving the little things

--- path_preloader.py
+++ path_preloader.py
@@ -7,71 +7,64 @@ ndo = Path("boot1.bin")
 ndc = Path("preloader_path/boot1.bin")
 print("Dev. Max_Goblin - 4pda")
 
-def auto_path_preloader(flag: bytes, fastboot_lock_state: int, file_size: int):
-    with open(Path("preloader_path/boot1.bin"), "r+b") as f:
+def auto_path_preloader(flag: bytes, fastboot_lock_state: bytes, file_size: int):
+    with open(ndc, "r+b") as f:
+        data = f.read()
+        
         # read code offset
-        f.seek(0x20d)
-        code_offset = f.read(0x1)[0] * 256
-        f.seek(0x21d)
-        code_offset1 = f.read(0x1)
-        f.seek(0x211)
-        code_offset2 = f.read(0x1)
-        f.seek(0x212)
-        code_offset3 = f.read(0x1)
-        f.seek(0x221)
-        code_offset4 = f.read(0x1)
-        f.seek(0x222)
-        code_offset5 = f.read(0x1)
+        code_offset = data[0x20d] * 256
+        code_offset1 = data[0x21d]
+        code_offset2 = data[0x211]
+        code_offset3 = data[0x212]
+        code_offset4 = data[0x221]
+        code_offset5 = data[0x222]
 
-        # read code 
-        f.seek(code_offset)
-        jumpr = f.read(file_size - 0x11120)
+        # read raw code
+        data_raw = data[code_offset : file_size - 0x3000]
 
         # writing zeros
         print(f"Write range zeros: 0x{code_offset:X}:0x2000")
         f.seek(code_offset)
         f.write(b'\x00' * (file_size - code_offset))
 
-        # writing code to new offset
+        # writing raw code to new offset
         if 0x2000 - code_offset >= 0:
-            print(f"Jupm offset code: 0x{code_offset:X} to 0x2000")
+            print(f"Jump offset code: 0x{code_offset:X} to 0x2000")
             f.seek(0x2000)
-            f.write(jumpr)
+            f.write(data_raw)
         else:
             print("Initial code indentation causes 0x2000. Script cannot work correctly")
             input("Press Enter to close: error 6")
             exit(6)
             
         # change code offset
+        print("--------------------\nChange BRLYT offset")
         print(f"0x20d: {int(code_offset/256):02x} -> 20")
         f.seek(0x20D)
         f.write(b"\x20")
-        print(f"0x21d: {code_offset1[0]:02x} -> 20")
+        print(f"0x21d: {code_offset1:02x} -> 20")
         f.seek(0x21D)
         f.write(b"\x20")
-
-        # I don't fully understand what this is,
-        # but it is responsible for executing the external flag block.
-        print(f"0x211: {code_offset2[0]:02x} -> 10")
+        print(f"0x211: {code_offset2:02x} -> 10")
         f.seek(0x211)      
         f.write(b"\x10")
-        print(f"0x212: {code_offset3[0]:02x} -> 10")
+        print(f"0x212: {code_offset3:02x} -> 10")
         f.seek(0x212)
         f.write(b"\x10")
-        print(f"0x221: {code_offset4[0]:02x} -> 10")
+        print(f"0x221: {code_offset4:02x} -> 10")
         f.seek(0x221)
         f.write(b"\x10")
-        print(f"0x222: {code_offset5[0]:02x} -> 10")
+        print(f"0x222: {code_offset5:02x} -> 10")
         f.seek(0x222)
         f.write(b"\x10")
         
         # Write a flag block
-        print("Write flag block to: 0x1000")
+        print("--------------------\nWrite flag block to: 0x1000")
         f.seek((0x1000))
         f.write(flag)
 
         # Path flag to unlock fastboot
-        print(f"Fastboot lock state: 0x{fastboot_lock_state:02x} -> 00")
+        print(f"Fastboot lock state: 0x{fastboot_lock_state[0]:02x} -> 00")
         f.seek(0x104C)
         f.write(b"\x00")
 
@@ -79,33 +72,34 @@ def auto_path_preloader(flag: bytes, fastboot_lock_state: int, file_size: int):
 
 def read_flag_block(file_size: int):
     pattern_flag = bytes.fromhex("41 4E 44 5F 52 4F 4D 49 4E 46 4F 5F 76")
-    with open(Path("preloader_path/boot1.bin"), "rb") as f: 
+    with open(ndc, "rb") as f: 
         data = f.read()
-        if data.find(pattern_flag) != -1:
+        patt_stat = data.find(pattern_flag)
+        if patt_stat != -1:
             print("Flag block find state: successfully")
-            f.seek(data.find(pattern_flag))
-            flag = f.read(0x77)
+            flag = data[patt_stat : patt_stat + 0x78]
+            patt_lock = patt_stat + 0x4C
+            fastboot_lock_state = data[patt_lock : (patt_lock + 1)]
         else:
             print("Magic numbers of flag block not found! Use manual instruction or contact me.")
             choice  = input("Do you wish to continue without the flag block? (y/n) ")
             if choice.lower() == "y":
                 flag = b""
+                fastboot_lock_state = 0x0
             else:
                 input("Press Enter to close: error 5")
                 exit(5)
-        f.seek(data.find(pattern_flag) + 0x4C)
-        fastboot_lock_state = f.read(0x1)
 
         if hex(fastboot_lock_state[0]) == "0x22":
             print("lock state: 22 (lock)")
         else:
              print("lock state: unlock")
 
-        return auto_path_preloader(flag, fastboot_lock_state[0], file_size)
+        return auto_path_preloader(flag, fastboot_lock_state, file_size)
 
 
 def check_validation():
-        file_size = ndo.stat().st_size
+        file_size = ndc.stat().st_size
         if file_size != normal_file_size:
             print(f"Expected file size - 0x400000 byte, received size - {hex(file_size)}.")
             choice = input("Ignore this and continue? (y/n) ")
@@ -141,9 +135,9 @@ def copy_preloader():
     makedirs("preloader_path", exist_ok=True)
     while True:
         try:
-            with open(ndo, "rb") as f_org:
-                with open(ndc, "wb") as f_pth:
-                    f_pth.write(f_org.read())
+            with open(ndo, "rb") as f_ndo:
+                with open(ndc, "wb") as f_ndc:
+                    f_ndc.write(f_ndo.read())
 
             print("boot1.bin found state: successfully")
 
@@ -152,9 +146,7 @@ def copy_preloader():
         except FileNotFoundError:
             print("boot1.bin found state: fail\nPlease use mtkclient to read your preloader (boot1).")
             choice = input("Repeat search? (y/n) ")
-            if choice.lower() == "y":
-                pass
-            else:
+            if choice.lower() != "y":
                 input("Press Enter to close: error 1")
                 exit(1)
 
